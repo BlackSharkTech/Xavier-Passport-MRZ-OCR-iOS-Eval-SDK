abddd06debf94bca303241847ba8eca3ba3a3810
Truc Dao <daotruc@hotmail.com>
2015-12-10 18:29:28 -0500

    Converted Xavier from static library to framework (Xavier.framework)

---
+++ XavierTestApplication/XavierDependencies/Xavier.framework/Headers/Xavier.h
@@ -0,0 +1,19 @@
+//
+//  Xavier.h
+//  Xavier
+//
+//  Created by Truc Dao on 12/9/15.
+//  Copyright © 2015 SimonComputing. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Xavier.
+FOUNDATION_EXPORT double XavierVersionNumber;
+
+//! Project version string for Xavier.
+FOUNDATION_EXPORT const unsigned char XavierVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Xavier/PublicHeader.h>
+
+
